74020bef52ff5111bd0baf3144d22d4b65ca4979
willer gonzalez velasco <willerp13@gmail.com>
2024-02-18 23:33:41 -0400

    excel

--- app/digitalpas/app/Exports/ExcelExport.php
+++ app/digitalpas/app/Exports/ExcelExport.php
@@ -3,13 +3,20 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
-use App\Models\Afiliacion; // Reemplaza "YourModel" con el nombre de tu modelo
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
+use App\Models\Afiliacion;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Sheet;
 
-class ExcelExport implements FromCollection
+class ExcelExport implements FromCollection, WithHeadings, WithColumnWidths
 {
-
-    private $idEstado; //nacio
-
+    private $idEstado;
+    use Exportable;
     public function __construct($idEstado)
     {
         $this->idEstado = $idEstado;
@@ -17,7 +24,30 @@ class ExcelExport implements FromCollection
 
     public function collection()
     {
-        //return Afiliados::all($idEstado); // Reemplaza "YourModel" con el nombre de tu modelo
-        return Afiliacion::where('edo', $this->idEstado)->get();   
+        return Afiliacion::where('edo', $this->idEstado)->get();
     }
+
+    public function headings(): array
+    {
+        return ['Registro de Requerimiento', 'Requerimiento', 'estado', 'Código del usuario', 'cédula', 'Primer nombre', 'primer apellido', 'fecha de creación', 'afiliación (planilla) digitalizada', 'cédula digitalizada'];
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 25, // Ancho de la columna A
+            'B' => 25, // Ancho de la columna B
+            'C' => 25, // Ancho de la columna C
+            'D' => 25, // Ancho de la columna A
+            'E' => 25, // Ancho de la columna B
+            'F' => 25, // Ancho de la columna C
+            'G' => 25, // Ancho de la columna A
+            'H' => 25, // Ancho de la columna B
+            'I' => 25, // Ancho de la columna C
+            'J' => 25, // Ancho de la columna C
+
+        ];
+    }
+
+   
 }
